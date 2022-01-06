df532a6b58bd4552080412209a5ca62f2e967dbb
eroauditore <alfa.fenrir@gmail.com>
2022-01-06 14:46:45 -0600

    New bulk item added

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -10,6 +10,7 @@ import TotalCard from "./TotalCard";
 import TableItems from "./TableItems";
 import ActionsCard from "./ActionsCard";
 import DialogCharge from "./DialogCharge";
+import DialogBulk from "./DialogBulk";
 
 const Item = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
@@ -26,14 +27,26 @@ const Home = () => {
   const [total, setTotal] = useState(0);
   const [cambio, setCambio] = useState(0);
   const [cantidad, setCantidad] = useState("");
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = useState(false);
+  const [openBulk, setOpenbulk] = useState(false);
+  const [bulkProduct, setBulkProduct] = useState({
+    id: 0,
+    description: "",
+    code: "",
+    sale_price: "",
+    purchase_price: "",
+    bulk_price: "",
+    bulk: false,
+    sub_total: 0,
+  });
   const inputChargeRef = useRef();
   const codeRef = useRef();
+  const bulkRef = useRef();
 
   const findProduct = () => {
     const { data: productos } = data;
     const product = productos.find((product) => product.code === barCode);
-    return product;
+    return product || {};
   };
 
   const handleOnChange = (e) => {
@@ -58,25 +71,61 @@ const Home = () => {
     }, 100);
   };
 
+  const handleOpenBulk = () => {
+    setOpenbulk(true);
+    setTimeout(() => {
+      bulkRef.current.focus();
+    }, 100);
+  };
+  const handleCloseBulk = () => {
+    setTotal(parseFloat(total) + parseFloat(bulkProduct.total));
+    setPurchase([...purchase, bulkProduct]);
+    setBarcode("");
+    setOpenbulk(false);
+    setTimeout(() => {
+      codeRef.current.focus();
+    }, 100);
+  };
+  const handleChangeBulk = (e) => {
+    setBulkProduct({
+      ...bulkProduct,
+      sub_total: e.target.value,
+      total: e.target.value,
+    });
+  };
+
   const handleKeyDown = (e) => {
     if (e.key === "Enter") {
-      const product = Object.create(findProduct());
-      if (product !== typeof "undefined") {
+      const product = { ...findProduct() };
+      if (Object.keys(product).length !== 0) {
         product.id = uuid();
         product.quantity = 1;
-        //Sub total is for bulk items
-        product.sub_total = product.sale_price;
-        product.total = product.sub_total * product.quantity;
-        setTotal(total + product.total);
-        setPurchase([...purchase, product]);
-        setBarcode("");
+        if (product.bulk === true) {
+          //display bulk dialog to get the price
+
+          setBulkProduct({ ...product });
+          handleOpenBulk();
+        } else {
+          //Sub total is for bulk items
+          product.sub_total = product.sale_price;
+          product.total = product.sub_total * product.quantity;
+          setTotal(parseFloat(total) + parseFloat(product.total));
+          setPurchase([...purchase, product]);
+        }
       }
+      setBarcode("");
+    }
+    if (e.key === "c" || e.key === "C") {
+      console.log("Cobrar");
+      setBarcode("");
     }
   };
 
   useEffect(() => {
     codeRef.current.focus();
   }, []);
+
+  const deleteItem = () => {};
 
   return (
     <Box sx={{ flexGrow: 1 }}>
@@ -118,6 +167,15 @@ const Home = () => {
         cambio={cambio}
         handleChange={handleChange}
         inputChargeRef={inputChargeRef}
+      />
+      <DialogBulk
+        handleClose={handleCloseBulk}
+        open={openBulk}
+        sale_price={bulkProduct.sale_price}
+        bulk_price={bulkProduct.bulk_price}
+        cantidad={cantidad}
+        handleChange={handleChangeBulk}
+        inputChargeRef={bulkRef}
       />
     </Box>
   );
